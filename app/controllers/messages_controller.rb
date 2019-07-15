4622dc38f34e6b99b7a44e540187585db2b6dd81
Guchi-Y <i25mas25@gmail.com>
2019-07-15 14:48:31 +0900

    Create message_index view
    チャット一覧画面であるindexビューを作成した

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :set_bird
+  
+  def index
+    @messages = @bird.messages.includes(:user).order("id DESC")
+  end
+
+  def new
+  end
+
+  def create
+    redirect_to "/birds/#{@bird.id}/messages" unless (Message.create(message_params)).save
+  end
+
+  private
+
+  def set_bird
+    @bird = Bird.find(params[:bird_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :image, :bird_id).merge(user_id: current_user.id)
+  end
 end
