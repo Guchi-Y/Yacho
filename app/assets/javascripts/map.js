74b91c830f9b99127a9b8e523ab779a03b5e91bf
Guchi-Y <i25mas25@gmail.com>
2019-07-19 13:00:33 +0900

    Update map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,14 +1,8 @@
-/*
-mapを関数の外で定義(codeAddressでも使うため)
-geocoderを用意
-*/
-
 let map
 let geocoder
 let mcs = [];
 
 function initMap(){
-  // geocoderを初期化
   geocoder = new google.maps.Geocoder()
 
   map = new google.maps.Map(document.getElementById('map'), {
@@ -16,7 +10,6 @@ function initMap(){
   zoom: 5
   });
 
-  // マップにマーカーを表示
   gon.points.forEach(function(point) {
      marker = new google.maps.Marker({
       position: new google.maps.LatLng( point.latitude, point.longitube ),
@@ -33,16 +26,10 @@ function initMap(){
   };
   new MarkerClusterer( map, mcs, mcOptions );
 
- //mapをクリックしたときのイベントを設定
  google.maps.event.addListener(map, 'click', mylistener);
- //クリックしたときの処理
  function mylistener(e){
-  //marker作成
   var marker = new google.maps.Marker();
-  //markerの位置を設定
-  //event.latLng.lat()でクリックしたところの緯度を取得
   marker.setPosition(new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()));
-  //marker設置
   marker.setMap(map);
   document.getElementById('lat').value = e.latLng.lat();
   document.getElementById('lng').value = e.latLng.lng();
@@ -50,13 +37,10 @@ function initMap(){
 }
 
 function codeAddress(){
-  // 入力を取得
   let inputAddress = document.getElementById('address').value;
 
-  // geocodingしたあとmapを移動
   geocoder.geocode( { 'address': inputAddress}, function(results, status) {
     if (status == 'OK') {
-  // map.setCenterで地図が移動
       map.setCenter(results[0].geometry.location);
       map.setZoom(16);
     } else {
