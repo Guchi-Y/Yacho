0d4c2790f4eca6e5f0a68063a95dc108b0d013cb
Guchi-Y <i25mas25@gmail.com>
2019-08-24 10:48:14 +0900

    Do not load file locally

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-  }
+if Rails.env.production? || Rails.env.staging?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
 
-  case Rails.env
-  when 'production'
-    config.fog_directory  = 'gy-baket'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gy-baket'
+    case Rails.env
+    when 'production'
+      config.fog_directory  = 'gy-baket'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gy-baket'
+    end
   end
 end
